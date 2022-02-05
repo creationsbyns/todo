93182cc30b6aeed7de5aca372da02a7f453ac36f
prateek <admin@creationsbyns.com>
2022-02-05 09:25:35 +0000

    db init

--- todolist/blog.py
+++ todolist/blog.py
@@ -4,18 +4,25 @@ from flask import (
 from werkzeug.exceptions import abort
 
 from todolist.auth import login_required
-from todolist.db import get_db
+from todolist.db import get_db 
+from todolist.db import init_db 
 
 bp = Blueprint('blog', __name__)
 
 @bp.route('/')
 def index():
     db = get_db()
-    posts = db.execute(
-        'SELECT p.id, title, body, created, author_id, username'
-        ' FROM task p JOIN user u ON p.author_id = u.id'
-        ' ORDER BY created DESC'
-    ).fetchall()
+    posts = {}
+    try:
+        posts = db.execute(
+            'SELECT p.id, title, body, created, author_id, username'
+            ' FROM task p JOIN user u ON p.author_id = u.id'
+            ' ORDER BY created DESC'
+        ).fetchall()
+    except:        
+        print("initilizing database")
+        init_db()
+
     return render_template('blog/index.html', posts=posts)
 
 
